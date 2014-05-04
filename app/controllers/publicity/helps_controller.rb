d2c2e50d937c4fc97dd3f858a6fcd85d2f3237ba
hfujimoto <hfujimoto@gmail.com>
2014-05-04 16:03:34 +0900

    updated faq system

--- app/controllers/publicity/helps_controller.rb
+++ app/controllers/publicity/helps_controller.rb
@@ -1,5 +1,9 @@
 class Publicity::HelpsController < InheritedResources::Base
+    respond_to :js, :only => [:show, :fade]
 
+  def fade
+    @help = Publicity::Help.find(params[:id])
+  end
   def build_resource_params
     [params.fetch(:publicity_help, {}).permit(:title, :content)]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   namespace :publicity do
-    resources :qas do
+    resources :qas, :helps do
          member do
            get 'fade'
          end
     end
-    resources :qa_categories, :helps
+    resources :qa_categories
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
